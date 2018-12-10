ec9311f2a20e2725bda13165c7302848f7c064bc
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2018-12-09 21:14:46 -0500

    update of if statements in react

---
+++ seasons/src/SeasonDislay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const SeasonDisplay = () => {
+	return (
+		<div>Season Display</div>
+	);
+};
+
+
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+
+	constructor(props) {
+		super(props);
+		//THIS is the ONLY TIME that we do a direct 
+		//Assigment to this.state inside a Constructor. 
+		this.state = { lat: null, errorMessage: '' };
+
+		window.navigator.geolocation.getCurrentPosition(
+		position => {
+			//Calling setState.
+			this.setState({ lat: position.coords.latitude});
+		},
+		err => {
+			this.setState({ errorMessage: err.message});
+			}
+
+	 	);
+	};
+
+	//react says we need to define render
+	render(){
+		if (this.state.errorMessage && !this.state.lat) {
+			return <div>{this.state.errorMessage}</div>
+		}
+
+		if(!this.state.errorMessage && this.state.lat){
+			return <div>{this.state.lat}</div>
+		}
+
+		return <div>Loading!....</div>
+	}
+};
+
+ReactDOM.render(
+	<App />,
+	document.querySelector('#root')
+);
